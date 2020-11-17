588bbc7827745a2c28e31c21553aa93e69afcb77
Miroslav Pavlak <mpavlak3@gmail.com>
2020-11-17 21:17:14 +0100

    Oprava ikon

---
+++ shop/database/migrations/2020_11_14_105240_add_fields_to_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddFieldsToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('surname')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('street')->nullable();
+            $table->integer('house_number')->nullable();
+            $table->string('town')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('surname');
+            $table->dropColumn('phone');
+            $table->dropColumn('street');
+            $table->dropColumn('house_number');
+            $table->dropColumn('town');
+        });
+    }
+}
---
+++ shop/resources/views/users/account.blade.php
@@ -0,0 +1,94 @@
+@extends('layout.app')
+
+@section('content')
+<section
+  class="p-3 lightContainer d-flex flex-column bg-light col-12 col-sm-8 col-md-6"
+>
+  <div class="d-flex align-items-center mb-3">
+    <h3>Môj účet</h3>
+  </div>
+
+  <form>
+    <div class="form-group">
+      <label for="exampleInputEmail1">Meno a priezvisko</label>
+      <input
+        type="email"
+        class="form-control col-12 col-md-10 col-xl-6"
+        id="exampleInputEmail1"
+        aria-describedby="emailHelp"
+        placeholder="Enter email"
+      />
+    </div>
+    <div class="form-group">
+      <label for="exampleInputEmail1">Mesto</label>
+      <input
+        type="email"
+        class="form-control col-12 col-md-10 col-xl-6"
+        id="exampleInputEmail1"
+        aria-describedby="emailHelp"
+        placeholder="Enter email"
+      />
+    </div>
+    <div class="form-group">
+      <label for="exampleInputEmail1">Ulica</label>
+      <input
+        type="email"
+        class="form-control col-12 col-md-10 col-xl-6"
+        id="exampleInputEmail1"
+        aria-describedby="emailHelp"
+        placeholder="Enter email"
+      />
+    </div>
+    <div class="form-group">
+      <label for="exampleInputEmail1">Tel. číslo</label>
+      <input
+        type="email"
+        class="form-control col-12 col-md-10 col-xl-6"
+        id="exampleInputEmail1"
+        aria-describedby="emailHelp"
+        placeholder="Enter email"
+      />
+    </div>
+    <div class="form-group">
+      <label for="exampleInputEmail1">E-mail</label>
+      <input
+        type="email"
+        class="form-control col-12 col-md-10 col-xl-6"
+        id="exampleInputEmail1"
+        aria-describedby="emailHelp"
+        placeholder="Enter email"
+      />
+    </div>
+    <button
+      type="submit"
+      class="btn btn-primary col-12 col-md-10 col-xl-6"
+    >
+      Zmeniť osobné údaje
+    </button>
+    <div class="form-group">
+      <label for="exampleInputPassword1">Heslo</label>
+      <input
+        type="password"
+        class="form-control col-12 col-md-10 col-xl-6"
+        id="exampleInputPassword1"
+        placeholder="Password"
+      />
+    </div>
+    <div class="form-group">
+      <label for="exampleInputPassword1">Potvrdiť heslo</label>
+      <input
+        type="password"
+        class="form-control col-12 col-md-10 col-xl-6"
+        id="exampleInputPassword1"
+        placeholder="Password"
+      />
+    </div>
+    <button
+      type="submit"
+      class="btn btn-primary col-12 col-md-10 col-xl-6"
+    >
+      Zmeniť heslo
+    </button>
+  </form>
+</section>
+@endsection
