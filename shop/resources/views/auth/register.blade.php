9419fc11279af3e2b68e128a7e315a73df01246a
Miroslav Pavlak <mpavlak3@gmail.com>
2020-11-17 23:00:27 +0100

    Registracia a prihlasenie v modal

--- shop/resources/views/auth/register.blade.php
+++ shop/resources/views/auth/register.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Registerácia') }}</div>
+                <div class="card-header">{{ __('Registrácia') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
--- shop/resources/views/layout/partials/nav.blade.php
+++ shop/resources/views/layout/partials/nav.blade.php
@@ -21,21 +21,18 @@
                         <a class="dropdown-item" href="../product/product_page.html"><i class="icons fa fa-cogs"></i> Príslušenstvo</a>
                     </div>
                 </li>
-                <!--<li class="nav-item">
-                    <a class="nav-link" href="#" data-toggle="modal" data-target="#logIn">Účet </a>
-                </li>-->
-
+                
                 <!-- Authentication Links -->
                 @guest
                     @if (Route::has('login'))
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            <a class="nav-link" href="#" data-toggle="modal" data-target="#logIn">Prihlásiť sa </a>
                         </li>
                     @endif
                     
                     @if (Route::has('register'))
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                            <a class="nav-link" href="#" data-toggle="modal" data-target="#signIn">Registrovať sa </a>
                         </li>
                     @endif
                 @else
@@ -72,77 +69,167 @@
     </nav>
 
     <!--Log in-->
-    <div class="fade modal" id="logIn" role="dialog">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header justify-content-between">
-                    <h4 class="modal-title">Log in</h4>
-                    <button type="button" class="close" data-dismiss="modal">
-                        &times;
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <form>
-                        <div class="form-group">
-                            <label>Email</label>
-                            <input type="text" class="form-control" placeholder="example@mail.com" />
-                        </div>
-                        <div class="form-group">
-                            <label>Heslo</label>
-                            <input type="password" class="form-control" placeholder="Heslo" />
-                        </div>
-                        <div class="d-flex justify-content-center mb-0">
-                            <button type="submit" class="btn btn-success btn-block">
-                                Prihlásiť sa
-                            </button>
-                        </div>
-                    </form>
-                </div>
+    <div class="fade modal mt-5" id="logIn" role="dialog">
+        <div class="container mt-4">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-header">{{ __('Prihlásenie') }}</div>
 
-                <div class="modal-footer d-flex justify-content-center">
-                    <a href="#" class="btn btn-secondary btn-block" data-toggle="modal" data-target="#signIn" data-dismiss="modal">Registrovať sa</a>
+                        <div class="card-body">
+                            <form method="POST" action="{{ route('login') }}">
+                                @csrf
+
+                                <div class="form-group row">
+                                    <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail') }}</label>
+
+                                    <div class="col-md-6">
+                                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                                        @error('email')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Heslo') }}</label>
+
+                                    <div class="col-md-6">
+                                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
+                                        @error('password')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <div class="col-md-6 offset-md-4">
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                            <label class="form-check-label" for="remember">
+                                                {{ __('Zapamätať si ma') }}
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="form-group row mb-0">
+                                    <div class="col-md-8 offset-md-4">
+                                        <button type="submit" class="btn btn-primary">
+                                            {{ __('Prihlásiť') }}
+                                        </button>
+
+                                        @if (Route::has('password.request'))
+                                            <a class="btn btn-link" href="{{ route('password.request') }}">
+                                                {{ __('Zabudli ste Heslo?') }}
+                                            </a>
+                                        @endif
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 
     <!--Sign in-->
-    <div class="fade modal" id="signIn" role="dialog">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header justify-content-between">
-                    <h4 class="modal-title">Sign in</h4>
-                    <button type="button" class="close" data-dismiss="modal">
-                        &times;
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <form>
-                        <div class="form-group">
-                            <label>Prihlasovací e-mail</label>
-                            <input type="text" class="form-control" placeholder="example@mail.com" />
+    <div class="fade modal mt-5" id="signIn" role="dialog">
+        <div class="container mt-5">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-header">{{ __('Registerácia') }}</div>
+
+                        <div class="card-body">
+                            <form method="POST" action="{{ route('register') }}">
+                                @csrf
+
+                                <div class="form-group row">
+                                    <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Meno') }}</label>
+
+                                    <div class="col-md-6">
+                                        <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+                                        @error('name')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Priezvisko') }}</label>
+
+                                    <div class="col-md-6">
+                                        <input id="surname" type="text" class="form-control @error('surname') is-invalid @enderror" name="surname" value="{{ old('surname') }}" required autocomplete="surname" autofocus>
+
+                                        @error('surname')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail') }}</label>
+
+                                    <div class="col-md-6">
+                                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+
+                                        @error('email')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Heslo') }}</label>
+
+                                    <div class="col-md-6">
+                                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+
+                                        @error('password')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Potvrďte Heslo') }}</label>
+
+                                    <div class="col-md-6">
+                                        <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                    </div>
+                                </div>
+
+                                <div class="form-group row mb-0">
+                                    <div class="col-md-6 offset-md-4">
+                                        <button type="submit" class="btn btn-primary">
+                                            {{ __('Registrovať') }}
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
                         </div>
-                        <div class="form-group">
-                            <label>Heslo</label>
-                            <input type="password" class="form-control" placeholder="Heslo" />
-                        </div>
-                        <div class="form-group">
-                            <label>Potvrdte heslo</label>
-                            <input type="password" class="form-control" placeholder="Potvrdte heslo" />
-                        </div>
-                        <div class="form-group">
-                            <label>Tel. číslo</label>
-                            <input type="text" class="form-control" placeholder="+421 900 123 456" />
-                        </div>
-                        <div class="d-flex justify-content-center mb-0">
-                            <button type="submit" class="btn btn-success btn-block">
-                                Sign in
-                            </button>
-                        </div>
-                    </form>
+                    </div>
                 </div>
             </div>
-        </div>
+        </div>    
     </div>
 
 </header>
