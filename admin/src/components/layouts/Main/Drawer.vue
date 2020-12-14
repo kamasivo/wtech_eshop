c4ee497b82bea4d8c059983234d7e66617442640
Miroslav Pavlak <mpavlak3@gmail.com>
2020-12-14 19:44:19 +0100

    Final commit

--- admin/src/components/layouts/Main/Drawer.vue
+++ admin/src/components/layouts/Main/Drawer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-deep-orange-3 full-height q-px-md q-py-lg">
+  <div class="bg-deep-orange-3 full-height q-px-md q-py-lg" color="grey-1">
     <div class="q-subheading q-mb-sm">Produkty</div>
     <q-list link class="no-border">
       <q-item class="q-body-1" to="/products/index">
@@ -8,6 +8,7 @@
       <q-item class="q-body-1" to="/products/create">
         <q-item-main>Vytvoriť produkt</q-item-main>
       </q-item>
+      <q-btn type="a" color="green" href="http://127.0.0.1:8000" >Naspäť do obchodu</q-btn>
     </q-list>
   </div>
 </template>
