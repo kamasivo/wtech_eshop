39047a823e4c949010e5bf05b4b645b4cdc2a4b8
Ivan Kamas <kamasivo@gmail.com>
2020-12-15 09:53:24 +0100

    before merge

--- admin/src/components/layouts/Main/Drawer.vue
+++ admin/src/components/layouts/Main/Drawer.vue
@@ -3,7 +3,7 @@
     <div class="q-subheading q-mb-sm">Produkty</div>
     <q-list link class="no-border">
       <q-item class="q-body-1" to="/products/index">
-        <q-item-main>Zoznam produktov</q-item-main>
+        <q-item-main class="text-weight-bolder white">Zoznam produktov</q-item-main>
       </q-item>
       <q-item class="q-body-1" to="/products/create">
         <q-item-main>Vytvoriť produkt</q-item-main>
